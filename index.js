67a4a7a85bcebd5254cf22ab37ed66052bfbc5a7
Davi <davilinfo@gmail.com>
2019-10-12 02:27:47 +0000

    returning

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const
     express = require('express'),
     bodyParser = require('body-parser'),
     log = require('npmlog'),
+    q = require('q'),
     http = require('./node_modules/xmlhttprequest/lib/XMLHttpRequest');
 
  var app = express();
  var   host = 'localhost';
  var   port = 10000;
+ var defer = q.defer(); 	
 
     app.use(bodyParser.json());
 
@@ -16,7 +18,7 @@ const
         res.setHeader('Access-Control-Allow-Origin', '*');
         log.info('API', '/api/forging');
 
-        return setForging(req.body);
+        res.status(200).json({ result: setForging(req.body) });
     });
 
     app.listen(port, function(){
@@ -35,18 +37,20 @@ function setForging(server){
     var forgingRequest = new http.XMLHttpRequest();
     var url = "http://localhost:".concat(server.port).concat("/api/node/status/forging");
 
+    var result = {};
     forgingRequest.onload = function(){
         console.log("forgingRequest.status: ".concat(forgingRequest.status));
         if (forgingRequest.status === 200){
             console.log("Forging changed");
             console.log(JSON.parse(forgingRequest.responseText).data);
-            return forgingRequest.responseText;
         }
     }
 
-    forgingRequest.open("PUT", url);
+    forgingRequest.open("PUT", url, false);
     forgingRequest.setRequestHeader("Cache-Control", "no-cache");
     forgingRequest.setRequestHeader("Content-Type", "application/json");
     forgingRequest.send(JSON.stringify(forgingData));
+
+    return JSON.parse(forgingRequest.responseText).data;
 }
 
